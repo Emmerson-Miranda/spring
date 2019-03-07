1a4c265b674763e48babf99f86b67a2c9c8a6d88
Emmerson <emmerson@emmerson.com>
2019-03-07 23:02:50 +0000

    queue creation and make properties explicit
    create queues and exchanges if they dont exist when queue subscription happen.

--- rabbitmq-client/src/main/java/edu/emmerson/microservice/rabbit/client/RabbitmqClientQueueProvider.java
+++ rabbitmq-client/src/main/java/edu/emmerson/microservice/rabbit/client/RabbitmqClientQueueProvider.java
@@ -5,8 +5,11 @@ import org.springframework.amqp.rabbit.connection.ConnectionFactory;
 import org.springframework.amqp.rabbit.core.RabbitTemplate;
 import org.springframework.amqp.rabbit.listener.SimpleMessageListenerContainer;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.beans.factory.annotation.Value;
 import org.springframework.context.annotation.Bean;
 import org.springframework.stereotype.Component;
+
+import com.rabbitmq.client.Channel;
 
 @Component
 public class RabbitmqClientQueueProvider {
@@ -16,6 +19,18 @@ public class RabbitmqClientQueueProvider {
 	
 	@Autowired
 	private RabbitTemplate rabbitTemplate;
+	
+	@Value( "${spring.rabbitmq.host}" )
+	private String rabbitmq_host;
+	
+	@Value( "${spring.rabbitmq.port}" )
+	private int rabbitmq_port;
+	
+	@Value( "${spring.rabbitmq.username}" )
+	private String rabbitmq_username;
+	
+	@Value( "${spring.rabbitmq.password}" )
+	private String rabbitmq_pwd;
 	
 	private SimpleMessageListenerContainer container;
 	
@@ -27,8 +42,48 @@ public class RabbitmqClientQueueProvider {
 	}
 	
 	public String addQueue(String name) {
+		registerQueue(name);
 		container.setQueues(new Queue(name, true));
 		return name + " added!";
+	}	
+	
+	/**
+	 * Check if queue exist, if is not create the queue and exchange.
+	 * @param name
+	 */
+	private void registerQueue(String name) {
+		boolean found = false;
+		for (String queue : container.getQueueNames()) {
+			if(queue.equals(name)) {
+				found = true;
+			}
+		}
+		if(!found) {
+			String queueName = name;
+			String exchangeName = name + ".exchange";
+			String routingKey = "";
+
+			com.rabbitmq.client.ConnectionFactory factory = new com.rabbitmq.client.ConnectionFactory();
+			factory.setUsername(rabbitmq_username);
+			factory.setPassword(rabbitmq_pwd);
+			factory.setVirtualHost("/");
+			factory.setHost(rabbitmq_host);
+			factory.setPort(rabbitmq_port);
+			
+			try {
+				com.rabbitmq.client.Connection conn = factory.newConnection();
+				
+				Channel channel = conn.createChannel();
+				channel.exchangeDeclare(exchangeName, "direct", true);
+				channel.queueDeclare(queueName , true, false, false, null);
+				channel.queueBind(queueName, exchangeName, routingKey);
+				channel.close();
+				
+				conn.close();
+			} catch (Throwable e) {
+				e.printStackTrace();
+			}
+		}
 	}
 	
 	public String removeQueue(String name) {
